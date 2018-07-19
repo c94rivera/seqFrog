67b7101927988eabae137b49d0a3e1bf60927aa2
carreq <c94rivera@gmail.com>
2018-07-18 20:23:21 -0400

    added input for trimmomatic .jar until it gets added to PATH on all computers

--- auto_trim.sh
+++ auto_trim.sh
@@ -3,10 +3,14 @@ input=$(yad --title="Trimmomatic" --text="Select input files:" \
 	--form \
 	--field="Working Directory":DIR \
 	--field="Forward Reads":FL \
-	--field="Reverse Reads":FL)
+	--field="Reverse Reads":FL \
+	--field="Trimmomatic jar file":FL)
 
 wdinput="$(echo "$input"| cut -d '|' -f 1)"
 forwreads="$(echo "$input"| cut -d '|' -f 2)"
 revreads="$(echo "$input" | cut -d '|' -f 3)"
+trimmo="$(echo "$input" | cut -d '|' -f 4)"
 
-#	java -jar trimmomatic-0.38.jar PE -threads 10 -phred33 $forwinput $revinput "$filename1"_paired.fastq "$filename1"_unpaired.fastq "$filename2"_paired.fastq "$filename2"_unpaired.fastq ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+export PATH=$PATH:$trimmo
+
+java -jar trimmomatic-0.38.jar PE -threads 10 -phred33 $forwreads $revreads "$filename1"_paired.fastq "$filename1"_unpaired.fastq "$filename2"_paired.fastq "$filename2"_unpaired.fastq ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
