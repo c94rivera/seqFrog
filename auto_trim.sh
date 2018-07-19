9fa7b9ba456279476c53719ca4c34a4e233870a3
carreq <c94rivera@gmail.com>
2018-07-18 20:14:29 -0400

    added parsing of yad use inputs

--- auto_trim.sh
+++ auto_trim.sh
@@ -5,10 +5,8 @@ input=$(yad --title="Trimmomatic" --text="Select input files:" \
 	--field="Forward Reads":FL \
 	--field="Reverse Reads":FL)
 
-echo $input
-
-# wdinput=$(cut -d '\n' -f1 /tmp/entries)
-# forwreads=$(cut -d '|' -f2 /tmp/entries)
-# revreads=$(cut -d '|' -f3 /tmp/entries)
+wdinput="$(echo "$input"| cut -d '|' -f 1)"
+forwreads="$(echo "$input"| cut -d '|' -f 2)"
+revreads="$(echo "$input" | cut -d '|' -f 3)"
 
 #	java -jar trimmomatic-0.38.jar PE -threads 10 -phred33 $forwinput $revinput "$filename1"_paired.fastq "$filename1"_unpaired.fastq "$filename2"_paired.fastq "$filename2"_unpaired.fastq ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
