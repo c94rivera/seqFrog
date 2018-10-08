356015588b68dd310f7ebb72e95e8bf8ea900ad7
Christopher <c94rivera@gmail.com>
2018-10-08 12:29:48 -0400

    added shell script to run corytophanes gonad data

---
+++ post_processing/corytophanes_gonad_pull_seq.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/litoria/Desktop/media/genomes/Corytophanes_percarinatus_unpublished/gonad/all_assemblies
+python3 /home/litoria/Documents/git/transcriptome_assembly/post_processing/pull_seqs_from_assembly.py mergedassemblies mergedassemblies.table
