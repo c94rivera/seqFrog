b3cfcbfa92900cafc3d00be169ffd645efb19129
Christopher <c94rivera@gmail.com>
2018-10-09 15:06:55 -0400

    moved print statements; added required arguments

--- post_processing/pull_seqs_from_assembly.py
+++ post_processing/pull_seqs_from_assembly.py
@@ -1,18 +1,18 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jun  7 14:27:49 2014
-
-@author: RDT
-adapted by: CAR
-"""
+#!/usr/bin/env python3
+#
+#Created on Sat Jun  7 14:27:49 2014
+#
+#@author: RDT
+#adapted by: CAR
+#
 
 from Bio import SeqIO
 import csv
 import sys
 from tqdm import tqdm
 
-def get_seqs(seqfile, matches, outfile, comparecolumn = None, keepcolumn = None):
-    #Open fasta infile and return iterator of SeqRecords with protein sequences.
+def get_seqs(seqfile, matches, outfile, comparecolumn, keepcolumn):
+    #Open fasta contigs and return iterator of SeqRecords with protein sequences.
     records = SeqIO.parse(seqfile, "fasta")
     seqlist=[]
     matchlist=[]
@@ -21,7 +21,7 @@ def get_seqs(seqfile, matches, outfile, comparecolumn = None, keepcolumn = None)
         for row in reader:
             matchlist.append(row)
         print("Blast file successfully read")
-    print("Comparing contigs to Blast file")
+    print("Comparing contigss to Blast file")
 
     proteinid = []
     for rec in tqdm(records):
@@ -44,40 +44,52 @@ def get_seqs(seqfile, matches, outfile, comparecolumn = None, keepcolumn = None)
                 continue
 
 
-    print("Writing annotated contigs")
+    print("Writing annotated contigss")
     with open(outfile, "w") as f:
         SeqIO.write(seqlist, f, "fasta")
 
 
 
 def main():
-    print("infile is", infile)
-    print("blast file is", blast_hits)
-    outfile = infile + "_matches.fasta"#blast_hits[:blast_hits.index(".")]+".fasta"
-    if comparecolumn:
-        print("Column used for comparison is", comparecolumn)
-    if keepcolumn:
-        print("Columns being kept include:", keepcolumn)
-    get_seqs(infile, blast_hits, outfile, comparecolumn, keepcolumn)
+    get_seqs(contigs, blast_hits, outfile, comparecolumn, keepcolumn)
     print ("outfile is", outfile)
+
+
 
 #grab arguments from console and pass them to python script
 import argparse
 parser = argparse.ArgumentParser()
 
 #argument tags
-parser.add_argument("-c", "--contig", dest = "infile", help = "Contig Input")
-parser.add_argument("-b", "--blast", dest = "blast_hits", help = "Blast Input")
-parser.add_argument("-comp", "--colcompare", dest = "comparecolumn", help = "Column for Comparison", default = "1")
-parser.add_argument("-keep", "--colkeep", dest = "keepcolumn", help = "Columns to Keep", default = "1,2,3,4")
+# parser.add_argument("contigs", help = "Contigs Input file")
+# parser.add_argument("blast_hits", help = "Blast Input")
+
+####flags for the required inputs
+req_grp = parser.add_argument_group(title='required arguments')
+req_grp.add_argument("-c", "--contig_file", dest = "contigs", required=True, help="Contigs input file ")
+req_grp.add_argument("-b", "--blast_hits", dest = "blast_hits", required=True, help="Blast Input File")
+####end of flags for the required inputs
+
+parser.add_argument("-comp", "--colcompare", dest = "comparecolumn", help = "Column for Comparison, starts counting at '0' (default: 1)", default = "1")
+parser.add_argument("-keep", "--colkeep", dest = "keepcolumn", help = "Columns to Keep, starts counting at '0' (default: 1, 2, 3, 4)", default = "1,2,3,4")
 
 args = parser.parse_args()
 
 #assign variables from command line arguments
-infile = args.infile
+contigs = args.contigs
 blast_hits = args.blast_hits
 comparecolumn = [int(x) for x in args.comparecolumn.split(",")]
 keepcolumn = [int(x) for x in args.keepcolumn.split(",")]
 
+print("contigs is", contigs)
+print("blast file is", blast_hits)
+outfile = contigs + "_matches.fasta"#blast_hits[:blast_hits.index(".")]+".fasta"
+print("Column used for comparison is", comparecolumn)
+print("Columns being kept include:", keepcolumn)
+
 #run main program
+from multiprocessing import pool
+import os
+
+
 main()
