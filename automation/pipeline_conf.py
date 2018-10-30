160bd701b007ea3e4c92110dbb9190ca50ef4795
Christopher <c94rivera@gmail.com>
2018-10-30 16:43:49 -0400

    rearranged and added all vars that would need to be changed by user

--- automation/pipeline_conf.py
+++ automation/pipeline_conf.py
@@ -1,6 +1,13 @@
 #input file location
 input_srx = "path/to/file"
 
+#binary locations
+megahit_bin = "megahit"
+abyss_bin = "abyss-pe"
+spades_bin = "~/Assembly_Tools/SPAdes-3.12.0/bin/spades.py" #location of spades binary on system
+transrate_bin = "/home/litoria/Assembly_Tools/transrate-1.0.3-linux-x86_64/transrate"
+blast_bin = "/home/litoria/NCBI_Tools/ncbi-blast-2.7.1+/bin/blastp"
+
 #custom blast library location
 custom_location = "/home/litoria/Assembly_Tools/Uniprot_library" 	#this might need to be removed if above input works
 
@@ -10,10 +17,6 @@ blast_name = "name_of_library"
 #final file path for assembly output
 filename = "final.contigs.fa"
 
-#location of megahit bin (if in $PATH just place the name of the bin)
-megahit = "megahit"
-blast_bin = "/home/litoria/NCBI_Tools/ncbi-blast-2.7.1+/bin/blastp"
-
-assembly_dir = f"{srx}.megahit_asm"
-
-working_dir = "/home/litoria/Documents/test"
+#location of forward and reverse reads
+forwreads = '/home/litoria/Desktop/media/genomes/Corytophanes_percarinatus_unpublished/hemipenes/3M-Nathan_S3_R1_001.fastq.gz'
+revreads = '/home/litoria/Desktop/media/genomes/Corytophanes_percarinatus_unpublished/hemipenes/3M-Nathan_S3_R2_001.fastq.gz'
