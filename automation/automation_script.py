efc53cd8428f69be256754d20aae05776fdca1bc
Christopher <c94rivera@gmail.com>
2018-10-31 16:26:11 -0400

    added skip if spades already exists, changed assembly folder names

--- automation/automation_script.py
+++ automation/automation_script.py
@@ -47,7 +47,7 @@ if pipeline_conf.abyss_bin:
     abyss_bin = pipeline_conf.abyss_bin
 
 if pipeline_conf.spades_bin:
-    spades_bin = pipeline_conf.abyss_bin
+    spades_bin = pipeline_conf.spades_bin
 
 if pipeline_conf.transrate_bin:
     transrate_bin = pipeline_conf.transrate_bin
@@ -91,57 +91,61 @@ def manual_input():
 #run megahit in single or paired end mode depending on amount of input files found
 def megahit1():     #use when manually inputing files
     global megahit_bin, forwreads, revreads
-    if len(glob.glob("*.megahit_asm")) >= 1:
+    if len(glob.glob("megahit_assembly")) >= 1:
         print("Megahit assembly folder already present.\nSkipping Megahit Assembly")
         sleep(5)
     else:
         if not revreads:
             print("Running Megahit in single-end mode")
             sleep(5)
-            subprocess.run(f"{megahit_bin} -r {forwreads} -o output.megahit_asm", shell=True)
+            subprocess.run(f"{megahit_bin} -r {forwreads} -o megahit_assembly", shell=True)
         else:
             print("Running Megahit in paired-end mode")
             sleep(5)
-            subprocess.run(f"{megahit_bin} -1 {forwreads} -2 {revreads} -o output.megahit_asm", shell=True)
+            subprocess.run(f"{megahit_bin} -1 {forwreads} -2 {revreads} -o megahit_assembly", shell=True)
 
 
 
 def megahit():      #use when input files need to be automatically detected
-    if len(glob.glob("*.megahit_asm")) >= 1:
+    if len(glob.glob("megahit_assembly")) >= 1:
         print("Megahit assembly folder already present")
         sleep(5)
     elif len(glob.glob("*.fastq")) == 2:
-        subprocess.run(f"{megahit_bin} -1 {forwreads} -2 {revreads} -o {forwreads}.megahit_asm", shell=True)
+        subprocess.run(f"{megahit_bin} -1 {forwreads} -2 {revreads} -o megahit_assembly", shell=True)
     elif len(glob.glob("*.fastq")) == 1:
-        subprocess.run(f"{megahit_bin} -r {forwreads} -o {forwreads}.megahit_asm", shell=True)
+        subprocess.run(f"{megahit_bin} -r {forwreads} -o megahit_assembly", shell=True)
     else:
         print("Error: Input files were not found")
 
 def abyss():
     global abyss_bin, forwreads, revreads
-    if len(glob.glob("abyss")) >= 1:
+    if len(glob.glob("abyss_assembly")) >= 1:
         print("Abyss assembly folder already present.\nSkipping Abyss assembly")
         sleep(5)
     else:
         cur_dir = os.getcwd()
-        os.mkdir(os.getcwd() + "/abyss")
-        os.chdir(os.getcwd() + "/abyss")
+        os.mkdir(os.getcwd() + "/abyss_assembly")
+        os.chdir(os.getcwd() + "/abyss_assembly")
         subprocess.run(f"{abyss_bin} j={coreamount} k=99 name=abyss_run in='{forwreads} {revreads}'", shell=True)
         os.chdir(cur_dir)
 
 def spades():
     global spades_bin, forwreads, revreads
-    subprocess.run(f"{spades_bin} -k 127 -1 {forwreads} -2 {revreads} -o spades_assembly", shell=True)
+    if len(glob.glob("spades_assembly")) >= 1:
+        print("Spades assembly folder already present.\nSkipping Spades assembly")
+        sleep(5)
+    else:
+        subprocess.run(f"{spades_bin} -k 127 -1 {forwreads} -2 {revreads} -o spades_assembly", shell=True)
 
 
 #combine all contig outputs with transrate
 def transrate():
     global transrate_bin
     #find megahit contig file
-    megahit_contig = (os.getcwd() + "/output.megahit_asm/final.contigs.fa")
+    megahit_contig = (os.getcwd() + "/megahit_assembly/final.contigs.fa")
 
     #find abyss contig file
-    abyss_contig = (os.getcwd() + "/abyss/abyss_run-contigs.fa")
+    abyss_contig = (os.getcwd() + "/abyss_assembly/abyss_run-contigs.fa")
 
     #find spades contig file
     spades_contig = (os.getcwd() + "/spade_assembly/contigs.fasta")
