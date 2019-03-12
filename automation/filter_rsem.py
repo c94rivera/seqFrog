d44703be292477e2e91974a3bd87a0293f83b446
CarReq <c94rivera@gmail.com>
2019-03-12 15:33:31 -0400

    Revert "added filter_rsem.py initial commit"
    This reverts commit f7df8de82b1b49453042afc2a7cded127ee37d5d.

--- automation/filter_rsem.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-grab lines based on e-value, length, highest expression
-format the first column so gene id is alone and searchable (maybe put this in 2nd column and replace)
-    search for uniprot gene id
-    group all lines with same gene id and perform function to that group
-print entire line to new file
