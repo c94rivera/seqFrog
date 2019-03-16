c2f52fcb97566f4b02d302638373ad12be3d1a1a
CarReq <c94rivera@gmail.com>
2019-03-16 08:28:10 -0400

    changed to remove unique uniprot_ids from df and print to new one

--- post_processing/filter_rsem.py
+++ post_processing/filter_rsem.py
@@ -14,8 +14,8 @@ import pandas as pd
 import numpy as np
 
 #open RSEM file into dataframe
-data1 = pd.read_csv('/home/christopher/Documents/1RSEM.genes.results', sep="\t")
-data2 = pd.read_csv('/home/christopher/Documents/2RSEM.genes.results', sep="\t")
+data1 = pd.read_csv('/home/litoria/Desktop/media/Christopher_thesis/1/rsem_outdir/RSEM.genes.results', sep="\t")
+data2 = pd.read_csv('/home/litoria/Desktop/media/Christopher_thesis/2/rsem_outdir/RSEM.genes.results', sep="\t")
 
 #split 'gene_id' column into contig and uniprot id
 new = data1['gene_id'].str.split('__', 5, expand=True)
@@ -37,6 +37,25 @@ data2['e-value'] = new2[4]
 data1['blast_match'] = new[5]
 data2['blast_match'] = new2[5]
 
+#remove uniprot_ids not found in opposite dataframe
+common = data1.merge(data2,on=['uniprot_id'])
+unique_in_1 = data1[(~data1.uniprot_id.isin(common.uniprot_id))]
+
+common2 = data2.merge(data1,on=['uniprot_id'])
+unique_in_2 = data2[(~data2.uniprot_id.isin(common.uniprot_id))]
+
+#remove unique uniprot ids from dataframes
+remove_uniques1 = pd.concat([data1, unique_in_1])
+data1 = remove_uniques1.drop_duplicates(keep=False)
+
+remove_uniques2 = pd.concat([data2, unique_in_2])
+data2 = remove_uniques2.drop_duplicates(keep=False)
+
+
+#create separate dataframes for each uniprot id and place all the dataframes into a dictionary
+diff_uniprot = dict(tuple(data1.groupby('uniprot_id')))
+diff_uniprot2 = dict(tuple(data2.groupby('uniprot_id')))
+
 #make array of all unique uniprot ids
 uniques = data1.uniprot_id.unique()
 uniques2 = data2.uniprot_id.unique()
@@ -44,10 +63,6 @@ uniques2 = data2.uniprot_id.unique()
 #find uniprot_ids not present in both dataframes actual
 s1 = np.setdiff1d(uniques, uniques2)
 s2 = np.setdiff1d(uniques2, uniques)
-
-#create separate dataframes for each uniprot id and place all the dataframes into a dictionary
-diff_uniprot = dict(tuple(data1.groupby('uniprot_id')))
-diff_uniprot2 = dict(tuple(data2.groupby('uniprot_id')))
 
 #create empty dataframe
 evalue_df = pd.DataFrame()
@@ -84,19 +99,20 @@ for i in uniques2:
     temp = data_temp[data_temp['TPM'] == max(data_temp['TPM'])]
     tpm_df2 = tpm_df2.append(temp, ignore_index=True)
 
-#create dataframe with missing uniprot uniprot_ids
-item_missing_in_2 = np.setdiff1d(uniques, uniques2)
-item_missing_in_1 = np.setdiff1d(uniques2, uniques)
+# #create dataframe with missing uniprot uniprot_ids
+# item_missing_in_2 = np.setdiff1d(uniques, uniques2)
+# item_missing_in_1 = np.setdiff1d(uniques2, uniques)
+#
+# missing2_df = pd.DataFrame(item_missing_in_2, columns=['uniprot_id'])
+# missing1_df = pd.DataFrame(item_missing_in_1, columns=['uniprot_id'])
 
-missing2_df = pd.DataFrame(item_missing_in_2, columns=['uniprot_id'])
-missing1_df = pd.DataFrame(item_missing_in_1, columns=['uniprot_id'])
 
-#add missing uniprot ids to dataframe
-missing1_df = pd.concat([pd.DataFrame([i], columns=['uniprot_id']) for i in item_missing_in_1], ignore_index=True, sort=True)
-evalue_df = evalue_df.append(missing1_df, ignore_index=True)
-
-missing2_df = pd.concat([pd.DataFrame([i], columns=['uniprot_id']) for i in item_missing_in_2], ignore_index=True, sort=True)
-evalue_df2 = evalue_df2.append(missing2_df, ignore_index=True)
+# #add missing uniprot ids to dataframe
+# missing1_df = pd.concat([pd.DataFrame([i], columns=['uniprot_id']) for i in item_missing_in_1], ignore_index=True, sort=True)
+# evalue_df = evalue_df.append(missing1_df, ignore_index=True)
+#
+# missing2_df = pd.concat([pd.DataFrame([i], columns=['uniprot_id']) for i in item_missing_in_2], ignore_index=True, sort=True)
+# evalue_df2 = evalue_df2.append(missing2_df, ignore_index=True)
 
 #reorganize the columns
 # 'gene_id', 'transcript_id(s)', 'length', 'effective_length', 'expected_count', 'TPM', 'FPKM', 'gene_id', 'uniprot_id', 'e-value', 'blast_match'
@@ -108,16 +124,22 @@ evalue_df2 = evalue_df2[cols]
 evalue_df = evalue_df.loc[:,~evalue_df.columns.duplicated()]
 evalue_df2 = evalue_df2.loc[:,~evalue_df2.columns.duplicated()]
 
+#sort Columns
+evalue_df = evalue_df.sort_values(by=['uniprot_id'])
+evalue_df2 = evalue_df2.sort_values(by=['uniprot_id'])
 #find common values between files
-s3 = list(set(uniques) & set(uniques2))
-s3
+# s3 = list(set(uniques) & set(uniques2))
+# s3
 # #find uniprot_ids not present in both dataframes actual
 # merged = evalue_df.merge(evalue_df2, indicator=True, how='outer')
 # merged[merged['_merge'] == 'right_only']
 
 #print dataframes to file
-evalue_df.to_csv('~/Documents/evalue_df.csv', sep='\t', index=False)
-evalue_df2.to_csv('~/Documents/evalue_df2.csv', sep='\t', index=False)
+evalue_df.to_csv('~/Documents/epithelium1.genes.results', sep='\t', index=False)
+evalue_df2.to_csv('~/Documents/epithelium2.genes.results', sep='\t', index=False)
+
+unique_in_1.to_csv('~/Documents/unique_in_1.csv', sep='\t', index=False)
+unique_in_2.to_csv('~/Documents/unique_in_2.csv', sep='\t', index=False)
 
 
 
